47eeb756d9aaeb604102c9dd3b13874d047d2ae3
rtom <rtom@freewheel.tv>
2019-02-15 13:20:00 -0500

    Add objective object

---
+++ flippy/objective.py
@@ -0,0 +1,18 @@
+from typing import Optional, Mapping
+from flippy.lp_variable import LpVariable
+from flippy.lp_expression import LpExpression, Numeric
+
+
+class Minimize:
+    pass
+
+
+class Maximize:
+    pass
+
+
+class Objective(LpExpression):
+    def __init__(self, name: str='', expression: Optional[Mapping[LpVariable, Numeric]] = None, constant: Numeric=0,
+                 sense=Minimize):
+        super(Objective, self).__init__(name, expression, constant)
+        self.sense = sense
