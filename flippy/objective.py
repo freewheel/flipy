3540f74d0d8878a202abf3d09ae796b2e180509a
rtom <rtom@freewheel.tv>
2019-02-15 13:39:18 -0500

    Add test for objective

--- flippy/objective.py
+++ flippy/objective.py
@@ -15,4 +15,17 @@ class Objective(LpExpression):
     def __init__(self, name: str='', expression: Optional[Mapping[LpVariable, Numeric]] = None, constant: Numeric=0,
                  sense=Minimize):
         super(Objective, self).__init__(name, expression, constant)
+        self._sense = None
+
         self.sense = sense
+
+    @property
+    def sense(self):
+        return self._sense
+
+    @sense.setter
+    def sense(self, sense):
+        if sense not in [Minimize, Maximize]:
+            raise ValueError("Sense must be one of <class 'flippy.objective.Minimize'>, "
+                             "<class 'flippy.objective.Maximize'> not %s" % sense)
+        self._sense = sense
---
+++ tests/test_objective.py
@@ -0,0 +1,29 @@
+import pytest
+from flippy.objective import Objective, Minimize, Maximize
+
+
+@pytest.fixture
+def objective(name='', expression=None, constant=0):
+
+    return Objective(name, expression, constant)
+
+
+class TestLpExpression(object):
+    def test_init(self):
+        obj = Objective(name='', expression=None, constant=0)
+        assert obj
+        assert obj.sense == Minimize
+        obj.sense = Maximize
+        assert obj.sense == Maximize
+
+    def test_bad_sense(self):
+        with pytest.raises(ValueError) as e:
+            Objective(name='', expression=None, constant=0, sense='')
+        assert "Sense must be one of <class 'flippy.objective.Minimize'>, <class 'flippy.objective.Maximize'> not " \
+               in str(e.value)
+
+        obj = Objective(name='', expression=None, constant=0)
+        with pytest.raises(ValueError) as e:
+            obj.sense = 'maximize'
+        assert "Sense must be one of <class 'flippy.objective.Minimize'>, <class 'flippy.objective.Maximize'> not " \
+               in str(e.value)
