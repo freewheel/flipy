1a60ba311ac6f1034121bc61539f14711134f77c
Lu，Lei <llu@freewheel.tv>
2019-02-20 15:28:04 -0500

    created LpProblem class

---
+++ flippy/lp_problem.py
@@ -0,0 +1,17 @@
+class LpProblem(Object):
+	def __init__(self, name, lp_constraint, lp_objective):
+		self.name = name
+		self.lp_constraint = lp_constraint
+		self.lp_objective = lp_objective
+
+
+	def solve(self):
+		pass
+
+
+	def read_lp(self):
+		pass
+
+	def write_lp(self):
+		pass
+
