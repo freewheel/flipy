1f54edfaefc816e10efdccc6e0c4f8d547044ba5
Peixuan Ding <pding@freewheel.tv>
2019-02-20 15:31:53 -0500

    Fixed indents

--- flippy/lp_problem.py
+++ flippy/lp_problem.py
@@ -1,17 +1,16 @@
-class LpProblem(Object):
-	def __init__(self, name, lp_constraint, lp_objective):
-		self.name = name
-		self.lp_constraint = lp_constraint
-		self.lp_objective = lp_objective
+class LpProblem(object):
 
+    def __init__(self, name, lp_constraint, lp_objective):
+        self.name = name
+        self.lp_constraint = lp_constraint
+        self.lp_objective = lp_objective
 
-	def solve(self):
-		pass
+    def solve(self):
+        pass
 
+    def read_lp(self):
+        pass
 
-	def read_lp(self):
-		pass
-
-	def write_lp(self):
-		pass
+    def write_lp(self):
+        pass
 
