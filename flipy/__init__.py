80a277f96c6082ec38c1e0fd930253369c5ce4a8
Peixuan Ding <pding@freewheel.tv>
2020-03-09 15:47:00 -0400

    Bumpbed version to 0.0.3

--- flipy/__init__.py
+++ flipy/__init__.py
@@ -16,4 +16,4 @@ except ImportError:
             raise ModuleNotFoundError('gurobipy not installed')
 from flipy.lp_objective import LpObjective, Minimize, Maximize
 
-version = '0.0.2'
+version = '0.0.3'
